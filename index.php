bbf1ee566122197731c0921038d1646b9fb2be7a
MaarcosDevs <maheru@alumnatflorida.es>
2025-12-12 10:22:06 +0100

    arreglando cosas

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-$_SESSION['Usuario'] = "Marcos";
+$nombre = $_POST['nombre'];
+$_SESSION['Usuario'] = $nombre;
 $_SESSION['Sesion'] = true;
 $color1 = $_POST['pag1'];
 $color2 = $_POST['pag2'];
@@ -8,6 +9,7 @@ $_SESSION['Color1'] = $color1;
 $_SESSION['Color2'] = $color2;
 ?>
 <form action="" method="POST">
+    <label>Ingresa tu numre: </label><input type="text" name="nombre"><br></br>
     <label>Elije el color de la pagina 1: </label><input type="text" name="pag1"><br></br>
     <label>Elije el color de la pagina 2: </label><input type="text" name="pag2"><br>
     <input type="submit">
